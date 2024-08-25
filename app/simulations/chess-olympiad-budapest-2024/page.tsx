78fb8863ce10a8a19ce0c9dd7589a131f692ece8
Caleb <cmwetherell@gmail.com>
2024-08-25 16:21:28 -0700

    add budapest olympiad

---
+++ app/simulations/chess-olympiad-budapest-2024/page.tsx
@@ -0,0 +1,21 @@
+import SimulationResults from "@/components/SimulationResults";
+import TournamentInfo from "@/components/TournamentInfo";
+
+const Sims: any = () => {
+    return (
+        <main className="flex-1 flex flex-col min-h-screen bg-white">
+        <div>
+            <TournamentInfo
+                name="Chess Olympiad Budapest 2024"
+                website="https://chessolympiad2024.fide.com/"
+                description="Country's battle it out 4v4 to find the best chess nation."
+                format="11 rounds of 4v4 team battles"
+            />
+            {/* <SimulationResults
+                eventTable="olympiad_2024"
+            /> */}
+        </div>
+        </main>
+    );
+    }
+export default Sims;
